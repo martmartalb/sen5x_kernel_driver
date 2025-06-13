83574e3bfe4ab7a9cc13f8bca3f0d7f8ce49210e
Your Name <you@example.com>
2025-06-13 15:29:38 +0200

    added read/write temperature compensation parameters

--- driver.c
+++ driver.c
@@ -22,15 +22,19 @@
 #include <linux/jiffies.h>
 
 struct sen5x_platform_data {
-	bool blocking_io;
-	bool high_precision;
+	struct temperature_compensation_data {
+        s16 offset;
+        s16 slope;
+        u16 time_constant;
+    } temperature_compensation;
 };
 
 /* measurements commands */
-static const unsigned char sen5x_cmd_start_measurement[]      = { 0x00, 0x21 };
-static const unsigned char sen5x_cmd_stop_measurement[]       = { 0x01, 0x04 };
+static const unsigned char sen5x_cmd_start_measurement[]       = { 0x00, 0x21 };
+static const unsigned char sen5x_cmd_stop_measurement[]        = { 0x01, 0x04 };
 static const unsigned char sen5x_cmd_start_gas_only_mode[]     = { 0x00, 0x37 };
 static const unsigned char sen5x_cmd_read_measured_values[]    = { 0x03, 0xC4 };
+static const unsigned char sen5x_cmd_read_temp_comp_params[]   = { 0x60, 0xB2 };
 /* other commands */
 static const unsigned char sen5x_cmd_clear_status_reg[]        = { 0xD2, 0x10 };
 
@@ -40,11 +44,12 @@ static const unsigned char sen5x_cmd_clear_status_reg[]        = { 0xD2, 0x10 };
 #define sen5x_CHANGE_MODE_WAIT_TIME_US     50000 // 50 ms
 #define sen5x_STOP_CMD_WAIT_TIME_US        200000 // 200 ms
 
-#define sen5x_CMD_LENGTH                   2
-#define sen5x_MEASUREMENT_RESPONSE_LENGTH  24
-#define sen5x_CRC8_LEN                     1
-#define sen5x_CRC8_POLYNOMIAL              0x31
-#define sen5x_CRC8_INIT                    0xFF
+#define sen5x_CMD_LENGTH                        2
+#define sen5x_MEASUREMENT_RESPONSE_LENGTH       24
+#define sen5x_TEMP_COMP_PARAMS_RESPONSE_LENGTH  9
+#define sen5x_CRC8_LEN                          1
+#define sen5x_CRC8_POLYNOMIAL                   0x31
+#define sen5x_CRC8_INIT                         0xFF
 
 DECLARE_CRC8_TABLE(sen5x_crc8_table);
 
@@ -158,6 +163,66 @@ out:
 		return ERR_PTR(ret);
 
 	return data;
+}
+
+static int temperature_compensation_params_update(struct sen5x_data *data){
+    int ret;
+    struct i2c_client *client = data->client;
+    char buf[sen5x_TEMP_COMP_PARAMS_RESPONSE_LENGTH];
+
+    ret = sen5x_read_from_command(client, data,
+                                  sen5x_cmd_read_temp_comp_params,
+                                  buf, sen5x_TEMP_COMP_PARAMS_RESPONSE_LENGTH,
+                                  data->wait_time);
+    if (ret)
+        return ret;
+
+    data->setup.temperature_compensation.offset = 
+        (s16) be16_to_cpup((__be16 *)buf);
+    data->setup.temperature_compensation.slope =
+        (s16) be16_to_cpup((__be16 *)(buf + 3));
+    data->setup.temperature_compensation.time_constant = 
+        (u16) be16_to_cpup((__be16 *)(buf + 3));
+    
+    return ret;
+}
+
+static u8 *write_word_with_crc(u8 *buf, u16 value)
+{
+    __be16 raw = cpu_to_be16(value);
+
+    memcpy(buf, &raw, sizeof(raw));
+    buf += sizeof(raw);
+
+    *buf = crc8(sen5x_crc8_table, 
+        buf - sizeof(raw), sizeof(raw), sen5x_CRC8_INIT);
+    return buf + 1;
+}
+
+static int temperature_compensation_params_write(struct device *dev,
+    size_t count, int offset, int slope, unsigned int time_constant)
+{
+    u8 buf[sen5x_CMD_LENGTH + sen5x_TEMP_COMP_PARAMS_RESPONSE_LENGTH];
+    u8 *position = buf;
+    int ret;
+
+    struct sen5x_data *data = dev_get_drvdata(dev);
+    struct i2c_client *client = data->client;
+
+    memcpy(position, sen5x_cmd_read_temp_comp_params, sen5x_CMD_LENGTH);
+    position += sen5x_CMD_LENGTH;
+
+    position = write_word_with_crc(position, offset);
+    position = write_word_with_crc(position, slope);
+    position = write_word_with_crc(position, time_constant);
+
+    ret = i2c_master_send(client, buf, position - buf);
+    if (ret != position - buf)
+        return ret < 0 ? ret : -EIO;
+
+    usleep_range(sen5x_READ_CMD_WAIT_TIME_US, sen5x_READ_CMD_WAIT_TIME_US + 1000);
+
+    return count;
 }
 
 // =============================================================
@@ -289,6 +354,152 @@ static ssize_t mode_show(struct device *dev,
 }
 
 
+static ssize_t temperature_compensation_params_show(struct device *dev,
+                struct device_attribute *attr, char *buf)
+{
+    struct sen5x_data *data = dev_get_drvdata(dev);
+    u8 index = to_sensor_dev_attr(attr)->index;
+    s16 integer, frac, scale;
+
+    switch(index) {
+        case 0: // offset
+            scale = 200; // scale for temperature compensation offset
+            integer = abs(data->setup.temperature_compensation.offset) / scale;
+            frac = abs(data->setup.temperature_compensation.offset % scale) * 1000 / scale;
+            return scnprintf(buf, PAGE_SIZE, "%s%d.%03d\n",
+                (data->setup.temperature_compensation.offset < 0) ? "-" : "",
+                integer, frac);
+        case 1: // slope
+            scale = 10000; // scale for temperature compensation offset
+            integer = abs(data->setup.temperature_compensation.slope) / scale;
+            frac = abs(data->setup.temperature_compensation.slope % scale) * 10000 / scale;
+            return scnprintf(buf, PAGE_SIZE, "%s%d.%04d\n",
+                (data->setup.temperature_compensation.slope < 0) ? "-" : "",
+                integer, frac);
+        case 2: // time constant
+            return scnprintf(buf, PAGE_SIZE, "%u\n",
+                data->setup.temperature_compensation.time_constant);
+        default:
+            return -EINVAL;
+    }
+}
+static int parse_float_value(const char *buf, u16 scale, u16 n_decimal_places, 
+        s16 * scaled_value)
+{
+    s16 integer = 0, frac = 0, frac_digits = 0, decimals_div, sign = 1;
+    int matched;
+    const char *dot;
+
+    const char *ptr = buf;
+
+    // Handle sign
+    if (*ptr == '-') {
+        sign = -1;
+        ptr++;
+    } else if (*ptr == '+') {
+        ptr++;
+    }
+
+    if(n_decimal_places == 3) {
+        matched = sscanf(ptr, "%hd.%3hd", &integer, &frac);
+        decimals_div = 1000;
+    }else if(n_decimal_places == 4) {
+        matched = sscanf(ptr, "%hd.%4hd", &integer, &frac);
+        decimals_div = 10000;
+    }
+    if (matched < 1)
+        return -EINVAL;
+    
+    // Find the decimal point in the input string
+    dot = strchr(ptr, '.');
+
+    if (dot) {
+        // Count digits after the dot (skip dot, count up to 3 digits)
+        const char *f = dot + 1;
+        while (isdigit(*f) && frac_digits < n_decimal_places) {
+            frac_digits++;
+            f++;
+        }
+    }
+
+    // Normalize fraction to 3 digits
+    if (n_decimal_places == 3) {
+        if (frac_digits == 1)
+            frac *= 100;
+        else if (frac_digits == 2)
+            frac *= 10;
+        // else leave as is if 3 digits
+    }else if (n_decimal_places == 4) {
+        if (frac_digits == 1)
+            frac *= 1000;
+        else if (frac_digits == 2)
+            frac *= 100;
+        else if (frac_digits == 3)
+            frac *= 10;
+        // else leave as is if 4 digits
+    }
+    *scaled_value = sign * (integer * scale + (frac * scale / decimals_div));
+    return 0;
+}
+
+static ssize_t temperature_compensation_params_store(struct device *dev,
+				     struct device_attribute *attr,
+				     const char *buf,
+				     size_t count)
+{
+    struct sen5x_data *data = dev_get_drvdata(dev);
+    u16 scale; // scale for temperature compensation offset
+    s16 scaled_value;
+    int ret;
+
+    switch (to_sensor_dev_attr(attr)->index) {
+        case 0: // offset
+            scale = 200; // scale for temperature compensation offset
+            ret = parse_float_value(buf, scale, 3, &scaled_value);
+            if (ret)
+                return ret;
+            break;
+        case 1: // slope
+            scale = 10000; // scale for temperature compensation slope
+            ret = parse_float_value(buf, scale, 4, &scaled_value);
+            if (ret)
+                return ret;
+            break;
+        case 2: // time constant
+            if (sscanf(buf, "%hd", &scaled_value) < 1)
+                return -EINVAL;
+            if (scaled_value < 0 || scaled_value > 128)
+                dev_err(dev, "Invalid time constant value: %hd. Must be in range 0-128.\n",
+                    scaled_value);
+                return -EINVAL;
+            break;
+        default:
+            return -EINVAL;
+    }
+
+    switch(to_sensor_dev_attr(attr)->index){
+        case 0: // offset
+            data->setup.temperature_compensation.offset = scaled_value;
+            break;
+        case 1: // slope
+            data->setup.temperature_compensation.slope = scaled_value;
+            break;
+        case 2: // time constant
+            data->setup.temperature_compensation.time_constant = (u16) scaled_value;
+            break;
+        default:
+            dev_err(dev, "Invalid index for temperature compensation parameters: %d\n", to_sensor_dev_attr(attr)->index);
+            return -EINVAL;
+    }
+
+    ret = temperature_compensation_params_write(dev,
+        count, data->setup.temperature_compensation.offset,
+        data->setup.temperature_compensation.slope,
+        data->setup.temperature_compensation.time_constant);
+
+    return ret;
+}
+
 static SENSOR_DEVICE_ATTR_RO(pm_1_0_input, measurement_input, pm_1_0);
 static SENSOR_DEVICE_ATTR_RO(pm_2_5_input, measurement_input, pm_2_5);
 static SENSOR_DEVICE_ATTR_RO(pm_4_0_input, measurement_input, pm_4_0);
@@ -297,6 +508,13 @@ static SENSOR_DEVICE_ATTR_RO(humidity_input, measurement_input, humidity);
 static SENSOR_DEVICE_ATTR_RO(temperature_input, measurement_input, temperature);
 static SENSOR_DEVICE_ATTR_RO(voc_index_input, measurement_input, voc_index);
 static SENSOR_DEVICE_ATTR_RO(nox_index_input, measurement_input, nox_index);
+
+static SENSOR_DEVICE_ATTR_RW(temperature_compensation_offset, 
+    temperature_compensation_params, 0);
+static SENSOR_DEVICE_ATTR_RW(temperature_compensation_slope, 
+    temperature_compensation_params, 1);
+static SENSOR_DEVICE_ATTR_RW(temperature_compensation_time_constant, 
+    temperature_compensation_params, 2);
 
 static SENSOR_DEVICE_ATTR_RW(mode, mode, 0);
 
@@ -309,6 +527,9 @@ static struct attribute *sen5x_attrs[] = {
 	&sensor_dev_attr_temperature_input.dev_attr.attr,
 	&sensor_dev_attr_voc_index_input.dev_attr.attr,
 	&sensor_dev_attr_nox_index_input.dev_attr.attr,
+	&sensor_dev_attr_temperature_compensation_offset.dev_attr.attr,
+	&sensor_dev_attr_temperature_compensation_slope.dev_attr.attr,
+	&sensor_dev_attr_temperature_compensation_time_constant.dev_attr.attr,
     &sensor_dev_attr_mode.dev_attr.attr,
 	NULL
 };
@@ -348,6 +569,7 @@ static int sen5x_probe(struct i2c_client *client)
 			      sen5x_CMD_LENGTH);
 	if (ret != sen5x_CMD_LENGTH)
 		return ret < 0 ? ret : -ENODEV;
+    usleep_range(20000, 30000);
 
     printk(KERN_INFO "sen5x driver: Malloc of device data...\n");
 	data = devm_kzalloc(dev, sizeof(*data), GFP_KERNEL);
@@ -355,8 +577,6 @@ static int sen5x_probe(struct i2c_client *client)
 		return -ENOMEM;
 
     printk(KERN_INFO "sen5x driver: Initializing device data...\n");
-	data->setup.blocking_io = false;
-	data->setup.high_precision = true;
 	data->mode = 1;
 	data->last_update = jiffies - msecs_to_jiffies(3000);
 	data->client = client;
@@ -369,6 +589,11 @@ static int sen5x_probe(struct i2c_client *client)
 	mutex_init(&data->data_lock);
 
     attribute_groups = sen5x_groups;
+
+    /* Update temperature compensation parameters */
+    ret = temperature_compensation_params_update(data);
+    if (ret)
+		return ret;
 
     printk(KERN_INFO "Registering hwmon device\n");
 	hwmon_dev = devm_hwmon_device_register_with_groups(dev,
